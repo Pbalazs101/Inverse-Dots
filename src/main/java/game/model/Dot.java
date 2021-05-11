ab27a3c18cbed0b0765760f565e2a87a94844554
Balázs Pintér <pbalazs101@gmail.com>
2021-05-11 18:31:12 +0200

    Dot.java added to project

---
+++ src/main/java/game/model/Dot.java
@@ -0,0 +1,12 @@
+package game.model;
+
+import javafx.beans.property.ObjectProperty;
+
+public class Dot {
+
+    private final DotType type;
+
+    public Dot(DotType type) {
+        this.type = type;
+    }
+}
