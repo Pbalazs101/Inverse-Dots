039783f4ded3114efba97d69a515f46fa1a8e900
Balázs Pintér <pbalazs101@gmail.com>
2021-05-12 16:31:51 +0200

    Position.java added to project

---
+++ src/main/java/game/model/Position.java
@@ -0,0 +1,8 @@
+package game.model;
+
+public record Position(int row, int col) {
+
+    public Position moveTo(Direction direction){
+        return new Position(row + direction.getRowChange(), col + direction.getColChange());
+    }
+}
