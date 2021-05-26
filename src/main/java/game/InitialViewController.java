9d448de37cca62f2fb05cec94ab94b19ef233a74
Balázs Pintér <pbalazs101@gmail.com>
2021-05-26 14:50:23 +0200

    Deleted now unused imports

--- src/main/java/game/InitialViewController.java
+++ src/main/java/game/InitialViewController.java
@@ -8,7 +8,6 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
