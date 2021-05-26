83158e56b057fbd37f84edc521b8341871917ce3
Balázs Pintér <pbalazs101@gmail.com>
2021-05-26 19:32:15 +0200

    Click to dismiss functionality added to leaderboard

--- src/main/java/game/LeaderboardViewController.java
+++ src/main/java/game/LeaderboardViewController.java
@@ -6,13 +6,19 @@ import java.util.List;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.stage.Stage;
 
 
 public class LeaderboardViewController {
@@ -35,6 +41,15 @@ public class LeaderboardViewController {
         ObservableList<Score> observableList = FXCollections.observableArrayList();
         observableList.addAll(scores);
         tableView.setItems(observableList);
+    }
+
+    @FXML
+    private void handleBackButton(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("/Initial.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
 }
